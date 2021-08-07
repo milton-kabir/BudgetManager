442ad2b078adb711c32bfb9b0d4e40cd38baedc1
milton-kabir <milton.cse.pust@gmail.com>
2021-08-07 10:30:26 +0600

    First Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -1,8 +1,23 @@
+//package budget;
 package com.kabir.milton;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Scanner sc=new Scanner(System.in);
+        double tot=0;
+        while(sc.hasNextLine()){
+            String st=sc.nextLine();
+            System.out.println(st);
+            String[] ar=st.split(" ");
+            String ss=ar[ar.length-1];
+            ss=ss.substring(1);
+            double d=Double.parseDouble(ss);
+            tot+=d;
+        }
+        System.out.println("Total: $"+tot);
     }
 }
